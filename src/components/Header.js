52946aceabf9709f0aacbc441e6b779813751255
burd5 <austinburdette5@gmail.com>
2022-10-30 12:41:32 -0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <div className="">
+        <h1 className="text-5xl text-center mt-10">Tea Time</h1>
+    </div>
+  )
+}
+
+export default Header
