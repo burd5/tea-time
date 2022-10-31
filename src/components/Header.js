1c95d6f45f08b31e45748935bb906c4be71c55c4
burd5 <austinburdette5@gmail.com>
2022-10-30 19:48:02 -0600

    worked on main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,22 @@ import './header.css'
 
 function Header() {
   return (
-    <div className="">
-        <h1 className="text-5xl text-center mt-10">Tea Time</h1>
+    <div className="flex justify-center">
+        <div className="mainLeft">
+            <h1 className="header text-center mt-10">Tea Time</h1>
+            <img className="teaImg" src="https://i.pinimg.com/originals/a5/8b/bd/a58bbd693cb5981b1717c5b184c4f898.gif" alt="" />
+        </div>
+        <div className="mainRight self-center ml-20 mt-40">
+                <button>
+                Find a Tea
+                </button>
+                <button>
+                Log In
+                </button>
+                <button>
+                Sign Up
+                </button>
+        </div>
     </div>
   )
 }
