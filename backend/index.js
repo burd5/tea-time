aaee04a37f3c4411472ed471dc4fb291bcb7de64
burd5 <austinburdette5@gmail.com>
2023-01-02 16:01:38 -0700

    CORs errors

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,11 @@ const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'example.com');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-}
+});
 
 require('./config/passport')(passport);
 
@@ -39,7 +37,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
-app.use(allowCrossDomain);
 app.use(cors({
   origin: "http://localhost:3000",
   credentials: true,
