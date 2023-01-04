dd3a7ed927e0431642e52bc787090b8d0b5bfc62
burd5 <austinburdette5@gmail.com>
2023-01-03 18:40:47 -0700

    changed https

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -28,7 +28,7 @@ function Collection() {
   const userID = localStorage.getItem('userID')
 
   useEffect(() => {
-    axios.get(`http://localhost:4000/collection`).then(res => {
+    axios.get(`https://localhost:4000/collection`).then(res => {
       setTeas(res.data);
     })
   }, [])
