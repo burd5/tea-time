7b36e0f17f466d29d73c232a76bccd136e19b634
burd5 <austinburdette5@gmail.com>
2023-01-05 07:41:54 -0700

    bcrypt error

--- backend/index.js
+++ backend/index.js
@@ -13,12 +13,11 @@ const Teas = require('./Models/Teas');
 const User = require('./Models/User');
 const { response } = require('express');
 const ObjectId = require('mongodb').ObjectId;
-const path = require('path')
 
 const app = express();
 
 require('./config/passport')(passport);
-require('dotenv').config({path: path.resolve('./config/.env')})
+require('dotenv').config({path: './config/.env'})
 
 // Middleware
 app.use(session({
