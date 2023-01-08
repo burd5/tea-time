bd23722fcdf650a0561c393ba625361dced3f7e5
burd5 <austinburdette5@gmail.com>
2023-01-08 16:43:40 -0700

    start

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://tea-time-backend.onrender.com',
   credentials: true,
   resave: false,
   saveUninitialized: false,
