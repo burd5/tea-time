f5a0dfddf9036484accb5ed2996072d71d0f7aa9
burd5 <austinburdette5@gmail.com>
2023-01-09 10:55:39 -0700

    changed dropdown colors

--- frontend/src/components/MobileMenu.js
+++ frontend/src/components/MobileMenu.js
@@ -53,7 +53,7 @@ export default function MobileMenu() {
         {user !== '' ? loggedIn.map((item, index) => (
           <Link to={item.link} key={index}>
           <ListItem>
-            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemIcon style={{color: 'purple'}}>{item.icon}</ListItemIcon>
             <ListItemText onClick={item.action} primary={item.name} />
           </ListItem>
           </Link>
